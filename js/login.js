40f12c33460d74d81eed2a3aa8455306e833be48
ulis3197 <rojasulises30@gmail.com>
2022-04-30 17:19:28 -0500

    conectar js y php

--- js/login.js
+++ js/login.js
@@ -1,33 +1,13 @@
-/*fetch('./php/conexion.php')
-.then(response => response.json())
-.then(data => {
-    console.log(data[0]);
-})
-.catch(error => console.error(error));*/
+$(window).ready(function() {
+    $("#formini").on('submit', function(e) {
+        e.preventDefault();
 
-$(document).ready(function () {
-    $("#inisesion").click(function () {
-        // alert("Entra");
-        // var xhr = new XMLHttpRequest();
-        // xhr.open('GET','./php/login.php',true);
-        // xhr.onload = function(){
-        //     if(xhr.status == 200){
-        //         var json = xhr.responseText
-        //         console.log(json);
-        //     }else{
-        //         console.log(error);
-        //     }
-        // }
-        // xhr.send();
+        let email = $("#email").val();
+        let password = $("#password").val();
+        console.log(email + "" + password);
 
-        var theobject = new XMLHttpRequest();
-        theobject.open('GET','./php/login.php',true);
-        theobject.setRequestHeader('Content-type','application/x-ww-form-urlencoded');
-        theobject.onreadystatechange = function (){
-            console.log(theobject.responseText);
-        }
-        theobject.send();
-
+        $.post('./php/login.php', { email, password }, function(data) {
+            console.log(data);
+        });
     });
 });
-
