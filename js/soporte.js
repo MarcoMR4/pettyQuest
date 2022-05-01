83e6a4f71aba77a8220c1e265b22236b883eb52d
FhightJPGalaxy <74805803+Fhight@users.noreply.github.com>
2022-05-01 15:16:21 -0500

    Add mensajeria
    Pestaña de mensajes

--- js/soporte.js
+++ js/soporte.js
@@ -1,12 +1,12 @@
 function mostrarConversacion(mostrar) {
     if (!mostrar) {
         $(".conversacion").show(500);
-        $(".card-footer").show(500);
+        $(".pieConversacion").show(500);
         return true;
     }
     else {
         $(".conversacion").hide(500);
-        $(".card-footer").hide(500);
+        $(".pieConversacion").hide(500);
         return false;
     }
 }
@@ -14,14 +14,13 @@ function mostrarConversacion(mostrar) {
 $(document).ready(function () {
     let mostrar = false;
     $(".conversacion").hide();
-    $(".card-footer").hide();
+    $(".pieConversacion").hide();
 
-    $("#btnSoporte").click(function () {
+    $(".btnSoporte").click(function () {
         if (!mostrar)
             mostrar = mostrarConversacion(mostrar);
 
         else
             mostrar = mostrarConversacion(mostrar);
-
     });
 });
