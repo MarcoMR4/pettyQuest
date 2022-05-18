4372eb940418f6cc8a870e5fb10bc2593bcfe713
ulis3197 <rojasulises30@gmail.com>
2022-05-17 20:34:01 -0500

    mostrar foto en input o algo asi

--- js/registro_mascotas.js
+++ js/registro_mascotas.js
@@ -21,6 +21,25 @@ $(window).ready(function() {
         
     });
 
+    function readURL(input) {
+        if (input.files && input.files[0]) { //Revisamos que el input tenga contenido
+            var reader = new FileReader(); //Leemos el contenido
+            $('#contenedordefoto').attr({'class':'caja'});
+            reader.onload = function(e) { //Al cargar el contenido lo pasamos como atributo de la imagen de arriba
+                $('#idFotoMas').attr({'src':e.target.result});
+            }
+            
+            reader.readAsDataURL(input.files[0]);
+        }
+        else{
+            
+        }
+    }
+
+    $("#nuevaFoto").change(function() { //Cuando el input cambie (se cargue un nuevo archivo) se va a ejecutar de nuevo el cambio de imagen y se verá reflejado.
+        readURL(this);
+    });
+
     $("#btnFoto").click(function (e) { 
         e.preventDefault();
         $("#nuevaFoto").click();
