ae6e3d73e586f83c28fd7fde535edb2fd52ee27a
MarcoMR4 <101607091+MarcoMR4@users.noreply.github.com>
2022-05-19 14:45:38 -0500

    Mandar ruta de la imagen a campo "foto", (Falta mover el archivo)

--- php/editar_mascota.php
+++ php/editar_mascota.php
@@ -4,27 +4,47 @@ require 'conexion_VeteAsosiones.php';
 $idMascota = isset($_POST['idMascota']) ? $_POST['idMascota'] : '';
 $nombre = isset($_POST['idNombre']) ? $_POST['idNombre'] : '';
 $raza = isset($_POST['idRaza']) ? $_POST['idRaza'] : '';
-$foto = isset($_POST['']) ? $_POST[''] : '';                      //obtener foto
+//$foto = isset($_FILES['fotoN']) ? $_FILES['fotoN'] : '';                 //obtener direccion de la foto 
+$foto = $_FILES['photo'];
 $edad = isset($_POST['idEdad']) ? $_POST['idEdad'] : '';
 $genero = isset($_POST['idGenero']) ? $_POST['idGenero'] : '';
 $tamaño = isset($_POST['idTamaño']) ? $_POST['idTamaño'] : '';
 $estatus = isset($_POST['idEstatus']) ? $_POST['idEstatus'] : '';
 $tipo = isset($_POST['idTipo']) ? $_POST['idTipo'] : '';
 
-/*echo "Id de la mascota: ".$idMascota; 
-echo "<br>Nombre de la mascota: ".$nombre;        //mostrar estos datos 
-echo "<br>Raza de la mascota: ".$raza;
-echo "<br>Edad: ".$edad;
-echo "<br>Genero: ".$genero;
-echo "<br>Tamaño: ".$tamaño;
-echo "<br>Estatus: ".$estatus;
-echo "<br>tipo: ".$tipo;*/
+var_dump($_FILES["photo"]);
 
-//mandar id de mascota 
-$nuevacon= new conexion_VeteAsosiones();
-$respuestajson=$nuevacon->editar_mascota($idMascota,$nombre,$raza,$foto,$edad,$genero,$tamaño,$estatus);
+echo "funciona aqui <br><br>";
+$hola = isset($_FILES['fotoNueva']['name']);
+echo $hola;
 
-//Header("location: http://localhost/pettyQuest/perfil_Mascota.html");
+$tmp_name = $foto['tmp_name'];
+$directorio_destino = "img/Mascotas";
+
+      $img_file = $foto['name'];
+      $img_type = $foto['type'];
+      echo 1;
+      echo "<br><br>Temporal: ".$tmp_name;
+      echo "<br><br>NUevo: ".$directorio_destino;
+
+//Si es una imagen 
+if((strpos($img_type, "jpg")) || (strpos($img_type, "jpeg")) || (strpos($img_type, "gif")) || (strpos($img_type, "png")) 
+ || (strpos($img_type, "webp"))){
+   echo "<br><br> Entro a formato"; 
+   $destino = $directorio_destino . '/' . $img_file;
+   echo "<br><br> DEstino: ".$destino; 
+   echo"<br><br><br> ".$tmp_name.",".$destino;
+   if($tmp_name!=""){
+       if(move_uploaded_file($tmp_name,$destino)){          //Poder mover el fichero a la carpeta de pryecto
+        //mandar id de mascota 
+        $nuevacon= new conexion_VeteAsosiones(); 
+        $respuestajson=$nuevacon->editar_mascota($idMascota,$nombre,$raza,$destino,$edad,$genero,$tamaño,$estatus);
+        Header("location: http://localhost/pettyQuest/perfil_Mascota.html");
+    }
+   }else{
+    echo "<br><br>No se movio el archivo";
+   }
+}
 
 
 
