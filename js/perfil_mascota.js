ca40f7bd6f2750291640c2fd892715575e5e7877
ulis3197 <rojasulises30@gmail.com>
2022-05-24 21:59:20 -0500

    editar imagen mascotas

--- js/perfil_mascota.js
+++ js/perfil_mascota.js
@@ -172,11 +172,30 @@ $(document).ready(function () {
         $("#idUbicacion").prop('readonly', true).prop('disabled', true).prop('required', false);
         $("#idTipo").prop('readonly', true).prop('disabled', true).prop('required', false);
     });
-    /*
-    $("#btnFoto").click(function (e) {
+
+    function readURL(input) {
+        if (input.files && input.files[0]) { //Revisamos que el input tenga contenido
+            var reader = new FileReader(); //Leemos el contenido
+            $('#contenedordefoto').attr({'class':'caja'});
+            reader.onload = function(e) { //Al cargar el contenido lo pasamos como atributo de la imagen de arriba
+                $('#idImagen').attr({'src':e.target.result});
+            }
+            
+            reader.readAsDataURL(input.files[0]);
+        }
+        else{
+            
+        }
+    }
+
+    $("#fotoNueva").change(function() { //Cuando el input cambie (se cargue un nuevo archivo) se va a ejecutar de nuevo el cambio de imagen y se verá reflejado.
+        readURL(this);
+    });
+
+    $("#btnFoto").click(function (e) { 
         e.preventDefault();
-        $("#nuevaFoto").click();
-    });*/
+        $("#fotoNueva").click();
+    });
 
     $("#btnAdoptado").click(function (e) {
         e.preventDefault();
--- php/editar_mascota.php
+++ php/editar_mascota.php
@@ -4,8 +4,6 @@ require 'conexion_VeteAsosiones.php';
 $idMascota = isset($_POST['idMascota']) ? $_POST['idMascota'] : '';
 $nombre = isset($_POST['idNombre']) ? $_POST['idNombre'] : '';
 $raza = isset($_POST['idRaza']) ? $_POST['idRaza'] : '';
-//$foto = isset($_FILES['fotoN']) ? $_FILES['fotoN'] : '';                 //obtener direccion de la foto 
-//$foto = $_FILES['photo'];
 $edad = isset($_POST['idEdad']) ? $_POST['idEdad'] : '';
 $genero = isset($_POST['idGenero']) ? $_POST['idGenero'] : '';
 $tamaño = isset($_POST['idTamaño']) ? $_POST['idTamaño'] : '';
@@ -13,46 +11,26 @@ $estatus = isset($_POST['idEstatus']) ? $_POST['idEstatus'] : '';
 $tipo = isset($_POST['idTipo']) ? $_POST['idTipo'] : '';
 $ubicacion = isset($_POST['idUbicacion']) ? $_POST['idUbicacion'] : '';
 
+if(isset($_FILES['photo'])){
 
-$nuevacon= new conexion_VeteAsosiones(); 
-$respuestajson=$nuevacon->editar_mascota($idMascota,$nombre,$raza,$edad,$genero,$tamaño,$estatus,$ubicacion);
-Header("location: http://localhost/pettyQuest/perfil_Mascota.html");
+  $tmpimg=$_FILES['photo']['tmp_name'];
+  $type=$_FILES['photo']['type'];
 
-/*var_dump($_FILES["photo"]);
+  if($type=='image/png'){
+    $type='.png';
+  }
+  else if($type=='image/jpg'){
+    $type='.jpg';   
+  }
+  else if($type=='image/jpeg'){
+    $type='.jpeg';
+  }
+  else{
+    $type='png';
+  }
 
-echo "funciona aqui <br><br>";
-$hola = isset($_FILES['fotoNueva']['name']);
-echo $hola;
-
-$tmp_name = $foto['tmp_name'];
-$directorio_destino = "img/Mascotas";
-
-      $img_file = $foto['name'];
-      $img_type = $foto['type'];
-      echo 1;
-      echo "<br><br>Temporal: ".$tmp_name;
-      echo "<br><br>NUevo: ".$directorio_destino;
-
-//Si es una imagen 
-if((strpos($img_type, "jpg")) || (strpos($img_type, "jpeg")) || (strpos($img_type, "gif")) || (strpos($img_type, "png")) 
- || (strpos($img_type, "webp"))){
-   echo "<br><br> Entro a formato"; 
-   $destino = $directorio_destino . '/' . $img_file;
-   echo "<br><br> DEstino: ".$destino; 
-   echo"<br><br><br> ".$tmp_name.",".$destino;
-   if($tmp_name!=""){
-       if(move_uploaded_file($tmp_name,$destino)){          //Poder mover el fichero a la carpeta de pryecto
-        //mandar id de mascota 
-        $nuevacon= new conexion_VeteAsosiones(); 
-        $respuestajson=$nuevacon->editar_mascota($idMascota,$nombre,$raza,$edad,$genero,$tamaño,$estatus);
-        Header("location: http://localhost/pettyQuest/perfil_Mascota.html");
-    }
-   }else{
-    echo "<br><br>No se movio el archivo";
-   }
-}*/
-
-
-
-
+  $nuevacon= new conexion_VeteAsosiones(); 
+  $respuestajson=$nuevacon->editar_mascota($idMascota,$nombre,$raza,$edad,$genero,$tamaño,$estatus,$ubicacion,$tmpimg,$type);
+  Header("location: http://localhost/pettyQuest/perfil_Mascota.html");
+}
 ?>
