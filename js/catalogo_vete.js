7a2acdbf3cf5fa5e96e9fe64cffe3fb2008bc521
KalanOne <onepunchalan@gmail.com>
2022-06-19 00:19:33 -0500

    Modal eliminar veterinaria

--- js/catalogo_vete.js
+++ js/catalogo_vete.js
@@ -8,9 +8,10 @@ $(window).ready(function () {
     success: function (response) {
       console.log(response);
       var relleno = "";
+      var modales = "";
       response.map(item => {
-        var nombrecom= item.nombreEncargado + " " + item.apellidoPEncargado + " " + item.apellidoMEncargado;
-        var Ubicacion= item.calle + " " + item.numero + ", " + item.ciudad; 
+        var nombrecom = item.nombreEncargado + " " + item.apellidoPEncargado + " " + item.apellidoMEncargado;
+        var Ubicacion = item.calle + " " + item.numero + ", " + item.ciudad;
         relleno += `
                 <div class="col-4 my-2">
                   <div class="card h-100 w-100 " style="width: 18rem;">
@@ -26,13 +27,33 @@ $(window).ready(function () {
                       <p class="card-text">Ubicacion: ${Ubicacion}</p>
                       <p class="card-text">Telefono: ${item.telefono}</p>
                       <p class="card-text">Correo: ${item.email}</p>
-                      <button type="button" class="btn btn-danger" id="btnEliminar" onclick="clickearVete('${item.claveAsociacionVeterinaria}')">Eliminar</button>
+                      <button type="button" class="btn btn-danger" id="btnEliminar" data-bs-toggle="modal" data-bs-target="#eliminar${item.claveAsociacionVeterinaria}">Eliminar</button>
                     </div>
                   </div>
                 </div>
                 `;
+        modales += `
+          <div class="modal fade" id="eliminar${item.claveAsociacionVeterinaria}" tabindex="-1" aria-labelledby="${item.claveAsociacionVeterinaria}ModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h3 class="modal-title" id="${item.claveAsociacionVeterinaria}ModalLabel">ALTO</h3>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <h4>Seguro que quieres eliminar la veterinaria ${item.nombre}</h4>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Cancelar</button>
+                  <button type="button" class="btn btn-danger" onclick="clickearVete('${item.claveAsociacionVeterinaria}')">Eliminar</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        `;
       })
       $("#contenido").html(relleno);
+      $("#modales").html(modales);
     }
   });
 });
