373a69a1ec9d8e4e3c4efff69dcf4be9329085f7
Ulis3197 <62312455+Ulis3197@users.noreply.github.com>
2022-10-09 13:19:32 -0500

    inicio sesion unificado

--- js/login.js
+++ js/login.js
@@ -1,84 +1,67 @@
-$(window).ready(function() {
-    var usuarioexiste = 0;
-    $('#formini').submit(function (e) { 
-        e.preventDefault();
-        var opcion=$('input:radio[name=btnradio]:checked').val()
-        console.log(opcion);
-        var datos = $(this).serializeArray();
+$(window).ready(function () {
+  var usuarioexiste = 0;
+  $("#formini").submit(function (e) {
+    e.preventDefault();
+    var opcion = $("input:radio[name=btnradio]:checked").val();
+    console.log(opcion);
+    var datos = $(this).serializeArray();
 
-        if(opcion=='1'){
-            $.ajax({
-
-                type: "POST",
-                url: "./php/login.php",
-                data: datos,
-                dataType: "JSON",
-                success: function (response) {
-
+    $.ajax({
+      type: "POST",
+      url: "./php/login.php",
+      data: datos,
+      dataType: "JSON",
+      success: function (response) {
+        //obtener tamaño del json
+        usuarioexiste = Object.keys(response).length;
+        if (usuarioexiste == 1) {
+          console.log(response);
+          $.getScript("./js/cambio_menu_tipo.js", function () {});
+          $("#btnsalirsesion").trigger("click");
+        } else {
+          $.ajax({
+            type: "POST",
+            url: "./php/loginVeterinaria.php",
+            data: datos,
+            dataType: "JSON",
+            success: function (response) {
+              //obtener tamaño del json
+              usuarioexiste = Object.keys(response).length;
+              if (usuarioexiste == 1) {
+                console.log(response);
+                $.getScript("./js/cambio_menu_tipo.js", function () {});
+                $("#btnsalirsesion").trigger("click");
+              } else {
+                $.ajax({
+                  type: "POST",
+                  url: "./php/iniciaradmin.php",
+                  data: datos,
+                  dataType: "JSON",
+                  success: function (response) {
                     //obtener tamaño del json
-                    usuarioexiste = (Object.keys(response).length);
+                    usuarioexiste = Object.keys(response).length;
                     if (usuarioexiste == 1) {
-                        console.log(response);
-                        $.getScript('./js/cambio_menu_tipo.js', function(){});
-                        $("#btnsalirsesion").trigger("click");
+                      console.log(response);
+                      window.location.href = "./catalogo_veterinarias.html";
+                      $("#btnsalirsesion").trigger("click");
+                    } else {
+                      $("#feedback-1").text("Usuario o Contraseña incorrectos");
                     }
-                    else{
-                        $("#feedback-1").text("Usuario o Contraseña incorrectos")
-                    }
-                },
-                error: function (response) {
-                    console.log(response);
-                }
-                
-            });
+                  },
+                });
+              }
+            },
+          });
         }
-        if(opcion=='2'){
-            $.ajax({
-                type: "POST",
-                url: "./php/loginVeterinaria.php",
-                data: datos,
-                dataType: "JSON",
-                success: function (response) {
-                   //obtener tamaño del json
-                    usuarioexiste = (Object.keys(response).length);
-                    if (usuarioexiste == 1) {
-                        console.log(response);
-                        $.getScript('./js/cambio_menu_tipo.js', function(){});
-                        $("#btnsalirsesion").trigger("click");
-                    }
-                    else{
-                        $("#feedback-1").text("Usuario o Contraseña incorrectos")
-                    }
-                }
-            });  
-        }
-        if(opcion=='3'){
-            $.ajax({
-                type: "POST",
-                url: "./php/iniciaradmin.php",
-                data: datos,
-                dataType: "JSON",
-                success: function (response) {
-                    //obtener tamaño del json
-                    usuarioexiste = (Object.keys(response).length);
-                    if (usuarioexiste == 1) {
-                        console.log(response);
-                        window.location.href = "./catalogo_veterinarias.html";
-                        $("#btnsalirsesion").trigger("click");
-                    }
-                    else{
-                        $("#feedback-1").text("Usuario o Contraseña incorrectos")
-                    }
-                }
-            });
-        }
-
-
-        /* Llamar otro js desde este mismo */
-        //$.getScript('./js/cambio_menu_tipo.js', function(){});
-
-        /* Dar click al boton de x en el registro desde aqui */
-        
+      },
+      error: function (response) {
+        console.log(response);
+      },
     });
-});
 
+    /* Llamar otro js desde este mismo */
+    //$.getScript('./js/cambio_menu_tipo.js', function(){});
+
+    /* Dar click al boton de x en el registro desde aqui */
+  });
+});
--- js/registro.js
+++ js/registro.js
@@ -1,121 +1,103 @@
 var localizacion = "";
 
 $(window).ready(function () {
+  var fecha = new Date();
+  fecha.setFullYear(fecha.getFullYear() - 18);
+  fecha.setMonth(11);
+  fecha.setDate(31);
+  $("#edad").attr("max", fecha.toISOString().split("T")[0]);
 
-    var fecha = new Date();
-    fecha.setFullYear(fecha.getFullYear() - 18);
-    fecha.setMonth(11);
-    fecha.setDate(31);
-    $("#edad").attr("max", fecha.toISOString().split('T')[0]);
+  $("#formregistro").submit(function (e) {
+    e.preventDefault();
+    var datos = $(this).serializeArray();
+    let password = $("#passwordr").val();
+    let password2 = $("#password2").val();
+    let correo = $("#emailr").val();
 
+    $.ajax({
+      type: "POST",
+      url: "./php/correoexistente.php",
+      data: datos,
+      dataType: "JSON",
+      success: function (response) {
+        console.log(response);
+        usuarioexiste = Object.keys(response).length;
+        console.log(usuarioexiste);
+        if (usuarioexiste >= 1) {
+          $("#feedback-correo").text("El correo ya esta en uso");
+        } else {
+          if (password == password2) {
+            console.log("Son iguales");
+          } else {
+            console.log("la contraseña no es la misma");
+          }
 
-    $('#formregistro').submit(function (e) {
-        e.preventDefault();
-        var datos = $(this).serializeArray();
-        let password = $("#passwordr").val();
-        let password2 = $("#password2").val();
-        let correo = $("#emailr").val();
-
-        $.ajax({
+          $.ajax({
             type: "POST",
-            url: "./php/correoexistente.php",
+            url: "./php/registro.php",
             data: datos,
             dataType: "JSON",
             success: function (response) {
-                console.log(response);
-                usuarioexiste = (Object.keys(response).length);
-                if (usuarioexiste >= 1) {
-                    $("#feedback-correo").text("El correo ya esta en uso")
-                }
-                else {
-
-                    if (password == password2) {
-                        console.log("Son iguales");
-                    } else {
-                        console.log("la contraseña no es la misma");
-                    }
-
-                    $.ajax({
-                        type: "POST",
-                        url: "./php/registro.php",
-                        data: datos,
-                        dataType: "JSON",
-                        success: function (response) {
-                            console.log(response);
-                            $.getScript('./js/cambio_menu_tipo.js', function () { });
-                            $("#btnsalirregistro").trigger("click");
-                            $("#abrirRegistroExitoso").trigger("click");
-                        }
-                    });
-                }
+              console.log(response);
+              $.getScript("./js/cambio_menu_tipo.js", function () {});
+              $("#btnsalirregistro").trigger("click");
+              $("#abrirRegistroExitoso").trigger("click");
             },
-            error: function (response) {
-                console.log(response);
-            }
-        });
-
-
-
-        /* Dar click al boton de x en el registro desde aqui */
-
-
+          });
+        }
+      },
+      error: function (response) {
+        console.log(response);
+      },
     });
 
-    $('#formregistrovet').submit(function (e) {
-        e.preventDefault();
-        var datos = $(this).serializeArray();
-        let passwordE = $("#passwordE").val();
-        let passwordE2 = $("#passwordE2").val();
-        let correo = $("#emailA").val();
+    /* Dar click al boton de x en el registro desde aqui */
+  });
 
-        $.ajax({
-            type: "POST",
-            url: "./php/correoexistente2.php",
-            data: datos,
-            dataType: "JSON",
-            success: function (response) {
+  $("#formregistrovet").submit(function (e) {
+    e.preventDefault();
+    var datos = $(this).serializeArray();
+    let passwordE = $("#passwordE").val();
+    let passwordE2 = $("#passwordE2").val();
+    let correo = $("#emailA").val();
+
+    $.ajax({
+      type: "POST",
+      url: "./php/correoexistente2.php",
+      data: datos,
+      dataType: "JSON",
+      success: function (response) {
+        console.log(response);
+        usuarioexiste = Object.keys(response).length;
+        if (usuarioexiste >= 1) {
+          $("#feedback-correo2").text("El correo ya esta en uso");
+        } else {
+          if (passwordE == passwordE2) {
+            console.log("Son iguales");
+            $.ajax({
+              type: "POST",
+              url: "./php/registro_asosiacionveterinaria.php",
+              data: datos,
+              dataType: "JSON",
+              success: function (response) {
                 console.log(response);
-                usuarioexiste = (Object.keys(response).length);
-                if (usuarioexiste >= 1) {
-                    $("#feedback-correo2").text("El correo ya esta en uso")
-                }
-                else {
-
-                    if (passwordE == passwordE2) {
-                        console.log("Son iguales");
-                        $.ajax({
-                            type: "POST",
-                            url: "./php/registro_asosiacionveterinaria.php",
-                            data: datos,
-                            dataType: "JSON",
-                            success: function (response) {
-                                console.log(response);
-                                $.getScript('./js/cambio_menu_tipo.js', function () { });
-                                $("#btnsalirregistro10").trigger("click");
-                                $("#abrirRegistroExitoso").trigger("click");
-                            }
-                        });
-                    } else {
-                        $("#feedback-correo2").text("La contraseña no concuerda")
-                        console.log(passwordE);
-                        console.log(passwordE2);
-                    }
-
-
-                }
-            },
-            error: function (response) {
-                console.log("ERROR DE AJAX");
-            }
-        });
-
-
-
-        /* Dar click al boton de x en el registro desde aqui */
-
-
+                $.getScript("./js/cambio_menu_tipo.js", function () {});
+                $("#btnsalirregistro10").trigger("click");
+                $("#abrirRegistroExitoso").trigger("click");
+              },
+            });
+          } else {
+            $("#feedback-correo2").text("La contraseña no concuerda");
+            console.log(passwordE);
+            console.log(passwordE2);
+          }
+        }
+      },
+      error: function (response) {
+        console.log("ERROR DE AJAX");
+      },
     });
+
+    /* Dar click al boton de x en el registro desde aqui */
+  });
 });
-
-
-
